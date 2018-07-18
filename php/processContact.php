4274d258dd581d322d1a7c615e9df0308ac97fce
basmoorkens <bas_moorkens@hotmail.com>
2018-07-18 22:37:13 +0200

    working version of contact form

--- php/processContact.php
+++ php/processContact.php
@@ -1,5 +1,4 @@
 <?php
-echo $_POST["name"];
 if(isset($_POST["name"])) {
 $name = $_POST["name"];
 }
@@ -15,57 +14,62 @@ $phone = $_POST["phone"];
 
 $to = "bas.moorkens@gmail.com";
 $subject="Eveland.io - contact form";
-$body = "On: " . date('Y/m/d H:i:s') .  "\n  " . $name . "\n" . " with email " . $contact . " and phone number " . $phone . " \nFilled in the contact form: " . $msg;
-$from="grotepiemel@basmoorkens.be";
-$from_name="bas test";
+$body = "<html><head></head><body>On: " . date('Y/m/d H:i:s') .
+        "\n<br /> Name:  " . $name .
+        "\n<br />" . "Email: " . $contact .
+        "\n<br /> Phone number: " . $phone .
+        "\n<br /> Filled in the contact form: <br />" . $msg .
+        "</body></html>";
+$from="contactform@eveland.io";
+$from_name="Eveland.IO contactform";
 
 $json = send_mailgun($to, $subject, $body,$from);
-
 function send_mailgun($to, $subject, $body, $from){
- 
-	$config = array();
- 
-	$config['api_key'] = "key-86494fe915f4b09e3f5e85efbc679435";
- 
-	$config['api_url'] = "https://api.mailgun.net/v2/sandbox225421aaa2cc4212aa5088cacf302930.mailgun.org/messages";
- 
-	$message = array();
- 
-	$message['from'] = $from;
- 
-	$message['to'] = $to;
- 
- 
-	$message['subject'] = $subject;
- 
-	$message['html'] = $body;
- 
-	$ch = curl_init();
- 
-	curl_setopt($ch, CURLOPT_URL, $config['api_url']);
- 
-	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
- 
-	curl_setopt($ch, CURLOPT_USERPWD, "api:{$config['api_key']}");
- 
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
- 
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
- 
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
- 
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
- 
-	curl_setopt($ch, CURLOPT_POST, true); 
- 
-	curl_setopt($ch, CURLOPT_POSTFIELDS,$message);
- 
-	$result = curl_exec($ch);
- 
-	curl_close($ch);
- 
-	return $result;
- 
+
+        $config = array();
+
+        $config['api_key'] = "key-86494fe915f4b09e3f5e85efbc679435";
+
+$config['api_url'] = "https://api.mailgun.net/v3/sandbox225421aaa2cc4212aa5088cacf302930.mailgun.org/messages";
+
+        $message = array();
+
+        $message['from'] = $from;
+
+        $message['to'] = $to;
+
+
+        $message['subject'] = $subject;
+
+        $message['html'] = $body;
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $config['api_url']);
+
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+        curl_setopt($ch, CURLOPT_USERPWD, "api:{$config['api_key']}");
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+        curl_setopt($ch, CURLOPT_POST, true);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS,$message);
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        return $result;
+
 }
 
 ?>
+
